9ad5eb61448e022b9feae1e7234e7eabd083fe55
fosterhayes-jpg <foster.hayes@cathcollege.school.nz>
2026-05-28 13:01:39 +1200

    i

--- area_2d.gd
+++ area_2d.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	# Check if the body that entered is the player
-	# Assumes your player script has 'class_name Player'
+	
 	if body is CharacterBody2D: 
 		print("Player died!")
 		# Reload the current scene to restart
